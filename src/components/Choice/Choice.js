75882aba18e74a1ac1b4ac580f92f3389335b3e7
Yavuz Selim Şerifoğlu <selimserifoglu03@gmail.com>
2022-01-07 13:56:01 +0300

    choice method added and basic game mechanic added

--- src/components/Choice/Choice.js
+++ src/components/Choice/Choice.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import { ChoiceButton, ChoiceContainer } from './Choice.styled';
 
-function Choice(){
+function Choice({ startGame }) {
+
+    const handleClick = (e) => {
+        const choice = e.target.value;
+        startGame(choice);
+    }
+
     return (
         <ChoiceContainer>
-            <ChoiceButton buttonColor="#5830CB">
+            <ChoiceButton onClick={handleClick} value="purple" buttonColor="#5830CB">
                 Purple Win 2x
             </ChoiceButton>
-            
-            <ChoiceButton>
+
+            <ChoiceButton onClick={handleClick} value="green">
                 Green Win 14x
             </ChoiceButton>
-            
-            <ChoiceButton buttonColor="#AC0000">
+
+            <ChoiceButton onClick={handleClick} value="red" buttonColor="#AC0000">
                 Red Win 2x
             </ChoiceButton>
         </ChoiceContainer>
--- src/page/MainPage/MainPage.js
+++ src/page/MainPage/MainPage.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 import { Amount, Header, History, Roll, Choice } from '../../components'
 import { Main } from "./MainPage.styled";
@@ -6,15 +6,39 @@ import { Main } from "./MainPage.styled";
 function MainPage() {
     const [money, setMoney] = useState(100);
     const [amount, setAmount] = useState(0);
+    const [choice, setChoice] = useState();
+
+    const startGame = (choice) => {
+        if(amount <= money){
+            setChoice(choice);
+            rollGame();
+        }else {
+            alert('You need to add more money !');
+        }
+    }
+
+    const rollGame = () => {
+        const gameResult = Math.floor(Math.random() * 15);
+        if(gameResult <= 7){
+            console.log('result' + gameResult);
+            console.log('purple');
+        }else if(gameResult >= 8 && gameResult < 14){
+            console.log('result' + gameResult);
+            console.log('red');
+        }else{
+            console.log('result' + gameResult);
+            console.log('green');
+        }
+    }
 
     return (
         <>
             <Header />
             <Main>
                 <History money={money} />
-                <Roll/>
-                <Amount money={money} setAmount={setAmount} amount={amount}/>
-                <Choice/>
+                <Roll />
+                <Amount money={money} setAmount={setAmount} amount={amount} />
+                <Choice startGame={(choice) => startGame(choice)} />
             </Main>
         </>
     );
